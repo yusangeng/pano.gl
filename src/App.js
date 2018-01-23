98562dbefc9ad316e297e0bdc45b434756be0082
yusangeng <yusangeng@outlook.com>
2018-01-24 02:25:21 +0800

    上传网站

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  componentDidMount() {
+    const video = document.createElement('video')
+    video.setAttribute('src', './city.mp4')
+    video.setAttribute('loop', true)
+    video.setAttribute('muted', true)
+    video.setAttribute('autoplay', true)
+
+    const { FramelessVideoViewer } = window.PanoGL
+    const viewer = new FramelessVideoViewer({
+      el: '#banner-viewer',
+      src: video,
+      camera: {
+        type: 'panini'
+      }
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return <div className="app">
+      <div id="banner-viewer" className="banner-viewer">
       </div>
-    );
+      <div className="npm-link">
+        <a href="https://www.npmjs.com/package/pano.gl">
+          <img src="https://nodei.co/npm/pano.gl.png?compact=true" />
+        </a>
+      </div>
+      <div className="text">
+        <h1>pano.gl</h1>
+        <h3>javascript & panorama.</h3>
+      </div>
+    </div>
   }
 }
 
-export default App;
+export default App
